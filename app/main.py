48bd051630fd641143edb9aff2ecdc6797532f6e
QUERE Eliott Francois Jean <eliott.quere@imt-atlantique.net>
2023-02-10 12:12:41 +0100

    Initial Pushv2

--- app/main.py
+++ app/main.py
@@ -2,5 +2,5 @@ from scraper.scrap import GetGithubInfos
 
 # Path: app/main.py
 if __name__ == '__main__':
-    repos = GetGithubInfos('https://github.com/hippiiee/osgint')
+    repos = GetGithubInfos('')
     repos.run()
